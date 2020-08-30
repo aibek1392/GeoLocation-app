701e24e4a31a9d143aff0a0f38cee08ec9e9bb6f
aibek <ozhorov@gmail.com>
2020-08-30 13:42:46 -0400

    rendering seasong depending on temp

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,35 @@
 import React from 'react'
 
-const SeasonDisplay = ({ latitude }) => {
-    return <div>Latitude: {latitude}</div>
+const seasonCofig = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr it is cold!",
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (latitude, month) => {
+    if (month > 2 && month < 9) {
+        return latitude > 0 ? 'summer' : 'winter';
+    } else {
+        return latitude > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+
+    const season = getSeason(props.latitude, new Date().getMonth());
+    const { text, iconName } = seasonCofig[season] // {text, iconName}
+
+    console.log(season)
+    return <div>
+                <i className={`${iconName} icon`} />
+                <h1>{text}</h1>
+                <i className={`${iconName} icon`} />
+           </div>
 }
 
 export default SeasonDisplay
